9e315a0a1c7ad1653f8c72dbd83bcf71ea07fbfd
villageworks <villageworks@gmail.com>
2024-01-27 03:15:08 +0900

    ver.1.00: サンプルショッピングの取り込みと動作確認

---
+++ src/main/java/com/example/demo/SpringKadai8EcLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKadai8EcLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKadai8EcLoginApplication.class, args);
+	}
+
+}
